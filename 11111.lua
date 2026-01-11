c7d6cd161fa4ee462fbb8d4f1c901d717c782397
Jack <3829174015@qq.com>
2026-01-11 22:19:53 +0800

    Add files via upload

---
+++ 11111.lua
@@ -0,0 +1,15 @@
+local MainTab = Window:Tab({
+    Title = "主页",
+    Icon = "house",
+    ShowTabTitle = true
+})
+
+MainTab:Paragraph({
+    Title = "无限脚本/∞",
+    Desc = "此脚本永久免费禁止倒卖 如果你是买来的那么恭喜你被骗了 此脚本半缝合半自制 永久更新 不跑路 感谢您的使用！",
+    Image = "user",
+    ImageSize = 70,
+    Thumbnail = "https://raw.githubusercontent.com/lool8/ugvgijgtugcgv/refs/heads/main/IMG_20260107_110528.jpg",
+    ThumbnailSize = 150,
+    Color = "Blue"
+})
